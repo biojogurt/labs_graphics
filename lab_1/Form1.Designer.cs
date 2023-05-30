b3078d521c0bd65e363a31cda1af61ea9ff3acf7
biojogurt <anathebeequeen@gmail.com>
2023-05-30 10:39:38 +0300

    elliple algo to circle

--- lab_1/Form1.Designer.cs
+++ lab_1/Form1.Designer.cs
@@ -30,10 +30,8 @@
         {
             pictureBox1 = new PictureBox();
             tableLayoutPanel1 = new TableLayoutPanel();
-            numericUpDown4 = new NumericUpDown();
             numericUpDown3 = new NumericUpDown();
             numericUpDown2 = new NumericUpDown();
-            label4 = new Label();
             label1 = new Label();
             label2 = new Label();
             button2 = new Button();
@@ -43,7 +41,6 @@
             colorDialog1 = new ColorDialog();
             ((System.ComponentModel.ISupportInitialize) pictureBox1).BeginInit();
             tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize) numericUpDown4).BeginInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown3).BeginInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown2).BeginInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown1).BeginInit();
@@ -53,11 +50,11 @@
             // 
             pictureBox1.BorderStyle = BorderStyle.FixedSingle;
             pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Location = new Point(125, 0);
+            pictureBox1.Location = new Point(109, 0);
             pictureBox1.Margin = new Padding(0);
             pictureBox1.Name = "pictureBox1";
-            tableLayoutPanel1.SetRowSpan(pictureBox1, 10);
-            pictureBox1.Size = new Size(675, 450);
+            tableLayoutPanel1.SetRowSpan(pictureBox1, 8);
+            pictureBox1.Size = new Size(691, 450);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Resize += pictureBox1_Resize;
@@ -69,25 +66,21 @@
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Controls.Add(numericUpDown4, 0, 7);
             tableLayoutPanel1.Controls.Add(numericUpDown3, 0, 5);
             tableLayoutPanel1.Controls.Add(numericUpDown2, 0, 3);
-            tableLayoutPanel1.Controls.Add(label4, 0, 6);
             tableLayoutPanel1.Controls.Add(pictureBox1, 1, 0);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
             tableLayoutPanel1.Controls.Add(label2, 0, 2);
-            tableLayoutPanel1.Controls.Add(button2, 0, 9);
+            tableLayoutPanel1.Controls.Add(button2, 0, 7);
             tableLayoutPanel1.Controls.Add(label3, 0, 4);
-            tableLayoutPanel1.Controls.Add(button1, 0, 8);
+            tableLayoutPanel1.Controls.Add(button1, 0, 6);
             tableLayoutPanel1.Controls.Add(numericUpDown1, 0, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Margin = new Padding(0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 10;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowCount = 8;
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
@@ -96,19 +89,10 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
             tableLayoutPanel1.Size = new Size(800, 450);
             tableLayoutPanel1.TabIndex = 1;
-            // 
-            // numericUpDown4
-            // 
-            numericUpDown4.AutoSize = true;
-            numericUpDown4.Dock = DockStyle.Top;
-            numericUpDown4.Location = new Point(3, 174);
-            numericUpDown4.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
-            numericUpDown4.Name = "numericUpDown4";
-            numericUpDown4.Size = new Size(119, 23);
-            numericUpDown4.TabIndex = 12;
-            numericUpDown4.Value = new decimal(new int[] { 100, 0, 0, 0 });
             // 
             // numericUpDown3
             // 
@@ -116,8 +100,9 @@
             numericUpDown3.Dock = DockStyle.Top;
             numericUpDown3.Location = new Point(3, 124);
             numericUpDown3.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown3.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(119, 23);
+            numericUpDown3.Size = new Size(103, 23);
             numericUpDown3.TabIndex = 11;
             numericUpDown3.Value = new decimal(new int[] { 80, 0, 0, 0 });
             // 
@@ -127,20 +112,11 @@
             numericUpDown2.Dock = DockStyle.Top;
             numericUpDown2.Location = new Point(3, 74);
             numericUpDown2.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown2.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(119, 23);
+            numericUpDown2.Size = new Size(103, 23);
             numericUpDown2.TabIndex = 10;
             numericUpDown2.Value = new decimal(new int[] { 110, 0, 0, 0 });
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(3, 153);
-            label4.Margin = new Padding(3);
-            label4.Name = "label4";
-            label4.Size = new Size(62, 15);
-            label4.TabIndex = 7;
-            label4.Text = "Радиус (y)";
             // 
             // label1
             // 
@@ -167,11 +143,11 @@
             button2.AutoSize = true;
             button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button2.Dock = DockStyle.Top;
-            button2.Location = new Point(3, 234);
+            button2.Location = new Point(3, 184);
             button2.Name = "button2";
-            button2.Size = new Size(119, 25);
+            button2.Size = new Size(103, 25);
             button2.TabIndex = 6;
-            button2.Text = "Построить эллипс";
+            button2.Text = "Построить круг";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
@@ -181,18 +157,18 @@
             label3.Location = new Point(3, 103);
             label3.Margin = new Padding(3);
             label3.Name = "label3";
-            label3.Size = new Size(62, 15);
+            label3.Size = new Size(45, 15);
             label3.TabIndex = 8;
-            label3.Text = "Радиус (x)";
+            label3.Text = "Радиус";
             // 
             // button1
             // 
             button1.AutoSize = true;
             button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
             button1.Dock = DockStyle.Top;
-            button1.Location = new Point(3, 203);
+            button1.Location = new Point(3, 153);
             button1.Name = "button1";
-            button1.Size = new Size(119, 25);
+            button1.Size = new Size(103, 25);
             button1.TabIndex = 5;
             button1.Text = "Выбрать цвет";
             button1.UseVisualStyleBackColor = true;
@@ -204,8 +180,9 @@
             numericUpDown1.Dock = DockStyle.Top;
             numericUpDown1.Location = new Point(3, 24);
             numericUpDown1.Maximum = new decimal(new int[] { 300, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(119, 23);
+            numericUpDown1.Size = new Size(103, 23);
             numericUpDown1.TabIndex = 9;
             numericUpDown1.Value = new decimal(new int[] { 110, 0, 0, 0 });
             // 
@@ -226,7 +203,6 @@
             ((System.ComponentModel.ISupportInitialize) pictureBox1).EndInit();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize) numericUpDown4).EndInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown3).EndInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown2).EndInit();
             ((System.ComponentModel.ISupportInitialize) numericUpDown1).EndInit();
@@ -238,13 +214,11 @@
 
         private PictureBox pictureBox1;
         private TableLayoutPanel tableLayoutPanel1;
-        private Label label4;
         private Label label1;
         private Label label2;
         private Button button2;
         private Label label3;
         private Button button1;
-        private NumericUpDown numericUpDown4;
         private NumericUpDown numericUpDown3;
         private NumericUpDown numericUpDown2;
         private NumericUpDown numericUpDown1;
