55a87bc8438fa3985aca81c09b01434a317a28a0
biojogurt <anathebeequeen@gmail.com>
2023-06-03 13:09:52 +0300

    add lab 3

---
+++ lab_3/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace KG3
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.bLight = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.tbRadius = new System.Windows.Forms.TextBox();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.tbDivisions = new System.Windows.Forms.TextBox();
+            this.lRadius = new System.Windows.Forms.Label();
+            this.lHeight = new System.Windows.Forms.Label();
+            this.lDivisions = new System.Windows.Forms.Label();
+            this.tlpOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpSide = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvLights = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Color = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.lLightsCount = new System.Windows.Forms.Label();
+            this.tbLightsCount = new System.Windows.Forms.TextBox();
+            this.tlpOuter.SuspendLayout();
+            this.tlpSide.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLights)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = true;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSize = true;
+            this.AnT.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AnT.Location = new System.Drawing.Point(0, 0);
+            this.AnT.Margin = new System.Windows.Forms.Padding(0);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(827, 647);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.Load += new System.EventHandler(this.AnT_Load);
+            this.AnT.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AnT_MouseMove);
+            // 
+            // bLight
+            // 
+            this.bLight.AutoSize = true;
+            this.bLight.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tlpSide.SetColumnSpan(this.bLight, 2);
+            this.bLight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bLight.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.bLight.Location = new System.Drawing.Point(25, 227);
+            this.bLight.Margin = new System.Windows.Forms.Padding(25, 35, 25, 35);
+            this.bLight.Name = "bLight";
+            this.bLight.Size = new System.Drawing.Size(213, 34);
+            this.bLight.TabIndex = 1;
+            this.bLight.Text = "Осветить";
+            this.bLight.Click += new System.EventHandler(this.bLight_Click);
+            // 
+            // colorDialog
+            // 
+            this.colorDialog.AnyColor = true;
+            this.colorDialog.FullOpen = true;
+            this.colorDialog.SolidColorOnly = true;
+            // 
+            // tbRadius
+            // 
+            this.tbRadius.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbRadius.Location = new System.Drawing.Point(128, 299);
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(132, 21);
+            this.tbRadius.TabIndex = 3;
+            this.tbRadius.Text = "5";
+            this.tbRadius.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbHeight.Location = new System.Drawing.Point(128, 326);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(132, 21);
+            this.tbHeight.TabIndex = 4;
+            this.tbHeight.Text = "5";
+            this.tbHeight.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // tbDivisions
+            // 
+            this.tbDivisions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbDivisions.Location = new System.Drawing.Point(128, 353);
+            this.tbDivisions.Name = "tbDivisions";
+            this.tbDivisions.Size = new System.Drawing.Size(132, 21);
+            this.tbDivisions.TabIndex = 5;
+            this.tbDivisions.Text = "5";
+            this.tbDivisions.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // lRadius
+            // 
+            this.lRadius.AutoSize = true;
+            this.lRadius.Location = new System.Drawing.Point(3, 302);
+            this.lRadius.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.lRadius.Name = "lRadius";
+            this.lRadius.Size = new System.Drawing.Size(47, 15);
+            this.lRadius.TabIndex = 6;
+            this.lRadius.Text = "Радиус";
+            // 
+            // lHeight
+            // 
+            this.lHeight.AutoSize = true;
+            this.lHeight.Location = new System.Drawing.Point(3, 329);
+            this.lHeight.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.lHeight.Name = "lHeight";
+            this.lHeight.Size = new System.Drawing.Size(51, 15);
+            this.lHeight.TabIndex = 7;
+            this.lHeight.Text = "Высота";
+            // 
+            // lDivisions
+            // 
+            this.lDivisions.AutoSize = true;
+            this.lDivisions.Location = new System.Drawing.Point(3, 356);
+            this.lDivisions.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.lDivisions.Name = "lDivisions";
+            this.lDivisions.Size = new System.Drawing.Size(119, 15);
+            this.lDivisions.TabIndex = 8;
+            this.lDivisions.Text = "Количество граней";
+            // 
+            // tlpOuter
+            // 
+            this.tlpOuter.AutoSize = true;
+            this.tlpOuter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tlpOuter.ColumnCount = 2;
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpOuter.Controls.Add(this.tlpSide, 1, 0);
+            this.tlpOuter.Controls.Add(this.AnT, 0, 0);
+            this.tlpOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpOuter.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tlpOuter.Location = new System.Drawing.Point(0, 0);
+            this.tlpOuter.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpOuter.Name = "tlpOuter";
+            this.tlpOuter.RowCount = 1;
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpOuter.Size = new System.Drawing.Size(1090, 647);
+            this.tlpOuter.TabIndex = 10;
+            // 
+            // tlpSide
+            // 
+            this.tlpSide.AutoSize = true;
+            this.tlpSide.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tlpSide.ColumnCount = 2;
+            this.tlpSide.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpSide.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpSide.Controls.Add(this.tbLightsCount, 1, 1);
+            this.tlpSide.Controls.Add(this.bLight, 0, 2);
+            this.tlpSide.Controls.Add(this.tbDivisions, 1, 5);
+            this.tlpSide.Controls.Add(this.lDivisions, 0, 5);
+            this.tlpSide.Controls.Add(this.tbHeight, 1, 4);
+            this.tlpSide.Controls.Add(this.lHeight, 0, 4);
+            this.tlpSide.Controls.Add(this.lRadius, 0, 3);
+            this.tlpSide.Controls.Add(this.tbRadius, 1, 3);
+            this.tlpSide.Controls.Add(this.dgvLights, 0, 0);
+            this.tlpSide.Controls.Add(this.lLightsCount, 0, 1);
+            this.tlpSide.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpSide.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tlpSide.Location = new System.Drawing.Point(827, 0);
+            this.tlpSide.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpSide.Name = "tlpSide";
+            this.tlpSide.RowCount = 6;
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpSide.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpSide.Size = new System.Drawing.Size(263, 647);
+            this.tlpSide.TabIndex = 11;
+            // 
+            // dgvLights
+            // 
+            this.dgvLights.AllowUserToAddRows = false;
+            this.dgvLights.AllowUserToDeleteRows = false;
+            this.dgvLights.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvLights.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dgvLights.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvLights.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLights.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y,
+            this.Z,
+            this.Color});
+            this.tlpSide.SetColumnSpan(this.dgvLights, 2);
+            this.dgvLights.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvLights.Location = new System.Drawing.Point(3, 3);
+            this.dgvLights.Name = "dgvLights";
+            this.dgvLights.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgvLights.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.dgvLights.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvLights.Size = new System.Drawing.Size(257, 150);
+            this.dgvLights.TabIndex = 10;
+            this.dgvLights.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvLights_CellContentClick);
+            // 
+            // X
+            // 
+            this.X.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.Width = 38;
+            // 
+            // Y
+            // 
+            this.Y.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            this.Y.Width = 37;
+            // 
+            // Z
+            // 
+            this.Z.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Z.HeaderText = "Z";
+            this.Z.Name = "Z";
+            this.Z.Width = 37;
+            // 
+            // Color
+            // 
+            this.Color.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Color.HeaderText = "Цвет";
+            this.Color.Name = "Color";
+            // 
+            // lLightsCount
+            // 
+            this.lLightsCount.AutoSize = true;
+            this.lLightsCount.Location = new System.Drawing.Point(3, 159);
+            this.lLightsCount.Margin = new System.Windows.Forms.Padding(3);
+            this.lLightsCount.Name = "lLightsCount";
+            this.lLightsCount.Size = new System.Drawing.Size(111, 30);
+            this.lLightsCount.TabIndex = 11;
+            this.lLightsCount.Text = "Количество\r\nисточников света";
+            this.lLightsCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbLightsCount
+            // 
+            this.tbLightsCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbLightsCount.Location = new System.Drawing.Point(128, 163);
+            this.tbLightsCount.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.tbLightsCount.Name = "tbLightsCount";
+            this.tbLightsCount.Size = new System.Drawing.Size(132, 21);
+            this.tbLightsCount.TabIndex = 12;
+            this.tbLightsCount.Text = "1";
+            this.tbLightsCount.TextChanged += new System.EventHandler(this.tb_TextChanged);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 647);
+            this.Controls.Add(this.tlpOuter);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "formMain";
+            this.Text = "Form1";
+            this.tlpOuter.ResumeLayout(false);
+            this.tlpOuter.PerformLayout();
+            this.tlpSide.ResumeLayout(false);
+            this.tlpSide.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLights)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.Button bLight;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.TextBox tbRadius;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.TextBox tbDivisions;
+        private System.Windows.Forms.Label lRadius;
+        private System.Windows.Forms.Label lHeight;
+        private System.Windows.Forms.Label lDivisions;
+        private System.Windows.Forms.TableLayoutPanel tlpOuter;
+        private System.Windows.Forms.TableLayoutPanel tlpSide;
+        private System.Windows.Forms.DataGridView dgvLights;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewButtonColumn Color;
+        private System.Windows.Forms.Label lLightsCount;
+        private System.Windows.Forms.TextBox tbLightsCount;
+    }
+}
+
---
+++ lab_3/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using System.Drawing;
+
+// моя: dabb
+
+namespace KG3
+{
+    public partial class formMain : Form
+    {
+        private double _Radius = 5;
+        private double _Height = 5;
+        private int _Slices = 5;
+
+        public formMain()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+            dgvLights.RowCount = 1;
+        }
+
+        private void AnT_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glClearColor(0, 0, 0, 1);
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            Gl.glEnable(Gl.GL_LIGHTING);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            double W = 3;
+            double H = W * AnT.Height / AnT.Width;
+            Gl.glFrustum(-W, W, -H, H, 2, 12);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+
+            Gl.glEnable(Gl.GL_LIGHT0);
+            Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, new float[] { 0, 0, 1, 1 });
+        }
+
+        private void AnT_MouseMove(object sender, MouseEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glPushMatrix();
+
+            Gl.glTranslated(0, 0, -8);
+            Gl.glRotated((e.X - AnT.Width) * 360 / AnT.Width, 0, 1, 0);
+            Gl.glRotated((e.Y - AnT.Height) * 360 / AnT.Height, 1, 0, 0);
+
+            Gl.glColor3f(1, 1, 1);
+            Glut.glutSolidCone(_Radius, _Height, _Slices, 1);
+
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            AnT.Invalidate();
+        }
+
+        private void dgvLights_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3 && DialogResult.OK == colorDialog.ShowDialog())
+                dgvLights.Rows[e.RowIndex].Cells[3].Value = colorDialog.Color;
+        }
+
+        private void bLight_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow r in dgvLights.Rows)
+                {
+                    Gl.glEnable(Gl.GL_LIGHT0 + r.Index);
+                    float[] pos =
+                    {
+                        float.Parse((string) r.Cells[0].Value),
+                        float.Parse((string) r.Cells[1].Value),
+                        float.Parse((string) r.Cells[2].Value),
+                        1
+                    };
+                    Gl.glLightfv(Gl.GL_LIGHT0 + r.Index, Gl.GL_POSITION, pos);
+
+                    float[] color =
+                    {
+                        ((Color) r.Cells[3].Value).R / (float) 255,
+                        ((Color) r.Cells[3].Value).G / (float) 255,
+                        ((Color) r.Cells[3].Value).B / (float) 255,
+                        1
+                    };
+                    Gl.glLightfv(Gl.GL_LIGHT0 + r.Index, Gl.GL_DIFFUSE, color);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+            }
+        }
+
+        private void tb_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(tbLightsCount.Text))
+                {
+                    dgvLights.RowCount = int.Parse(tbLightsCount.Text);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+                tbLightsCount.Clear();
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(tbRadius.Text))
+                    _Radius = double.Parse(tbRadius.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+                tbRadius.Clear();
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(tbHeight.Text))
+                    _Height = double.Parse(tbHeight.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+                tbHeight.Clear();
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(tbDivisions.Text))
+                    _Slices = int.Parse(tbDivisions.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка входных данных!");
+                tbDivisions.Clear();
+            }
+        }
+    }
+}
